8fb3fa6527e57274bed59594b282a95ac03bf6eb
inlee <einable@gmail.com>
2018-10-03 20:47:17 +0900

    Update conf

--- certbot-new.sh
+++ certbot-new.sh
@@ -9,4 +9,11 @@ fi
 
 # PORT_CERTBOT=$(grep PORT_CERTBOT .env | cut -d '=' -f2)
 
-docker-compose -f $1 run --rm 
+docker-compose -f $1 run --rm certbot certonly \
+  --standalone \
+  --email $USER_EMAIL
+  -d $URL_APP \
+  -d $URL_PHPMYADMIN \
+  --agree-tos \
+  --non-interactive \
+  --http-01-port=$PORT_CERTBOT
