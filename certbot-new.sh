b4ee390ae9161fb8826a13a11598e5bedc7547a1
inlee <einable@gmail.com>
2018-10-03 15:30:00 +0900

    Add certbot-new.sh

---
+++ certbot-new.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# certbot-new.sh userEmail certDomain
+
+if [ -z "$1" ]; then
+  echo 'Email required.'
+  echo 'Usage: ./certbot-new.sh userEmail certDomain'
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  echo 'Domain required.'
+  echo 'Usage: ./certbot-new.sh userEmail certDomain'
+  exit 1
+fi
+
+userEmail=$1
+certDomain=$2
+
+docker-compose run --rm certbot certonly --standalone --email $userEmail --agree-tos --non-interactive -d $certDomain --http-01-port=8888
